ee9cbd9c07d9563f968edb6bbfb5304140caa2f2
Brian O'Reilly <fade@deepsky.com>
2012-03-06 15:23:55 -0500

    move the code from the original project into the pappy.lisp file generated by quickproject.

--- pappy.lisp
+++ pappy.lisp
@@ -4,3 +4,33 @@
 
 ;;; "pappy" goes here. Hacks and glory await!
 
+(defun deliver-response (resp json)
+  (format resp json))
+  
+(defun render (response)
+  (json:encode-json-to-string response))
+  
+(defun path-elements (path)
+  (split-sequence #\/ path))
+
+; Handles routing to models
+(defun response-to (request)
+  (let
+    (
+      (path (puri:uri-path (toot:request-uri request)))
+    )
+    (let
+      (
+        (model (singular-of (nth 1 (path-elements path))))
+        (key                (nth 2 (path-elements path)))
+      )
+      (find-all-or-one model key))))
+
+; find either all the objects or one (if there's an optional key, show just that one)
+(defun find-all-or-one (model &optional key))
+
+(defun http-handler (request)
+  (let ((resp (toot:send-headers request)))
+    (deliver-response resp (render (response-to request)))))
+
+(toot:start-server :port 8080 :handler 'http-handler)
